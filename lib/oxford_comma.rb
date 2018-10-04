4242a670d99ac9472e707836092abec700f971d2
Sara Kantor <sara.kantor.nv@gmail.com>
2018-10-04 22:30:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
   if array.length == 1 || array.length == 0
     array.join
   else 
-    array.last = 
+    final = array.pop
+    final = "and " + final
     array = array.join(", ")
     
   end 
